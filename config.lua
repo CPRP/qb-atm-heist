126fb337c93d57b0d13ac0f45290b0d3a66497ca
donutsforains <113471914+donutsforains@users.noreply.github.com>
2022-12-03 23:55:24 -0500

    updated

--- config.lua
+++ config.lua
@@ -13,11 +13,17 @@ Config.ATMModels = {
 Config.BankingTarget = true -- Set to true if you want qb-atms to have its option on the atm as well.
 Config.RemoveItemOnFailure = true
 Config.HackItem = "trojan_usb"
-Config.Dispatch = 'qb-dispatch' -- qb-dispatch, qb-default, ps-dispatch or "custom" @ 
-Config.PoliceChance = 75 -- % Chance for Police to be called.
+Config.Dispatch = 'ps-dispatch' -- qb-dispatch, qb-default, ps-dispatch or "custom" @ 
+Config.PoliceChance = 100 -- % Chance for Police to be called.
 Config.FailCooldown = true -- If false, failure will not trigger cooldown.
 
 
-Config.PhysicalItem = false
-Config.MoneyReward = true
+Config.PhysicalItem = true
+Config.MoneyReward = false
 Config.RewardCashAmount = 5000
+
+-- Added for new adding marked bills
+Config.MinAmount = 1 -- 1 bag
+Config.MaxAmount = 3 -- 5 bags
+Config.MinWorth = 500 -- $500 minimum
+Config.MaxWorth = 2000 -- $2000 maximum
